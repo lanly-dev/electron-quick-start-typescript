a3ef6d686259f0947bff2001c5b6976864a64544
lanly_dev <lanly-dev@users.noreply.github.com>
2019-09-22 19:42:51 -0700

    Update, use pug, add configs for vscode

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,63 @@
-import { app, BrowserWindow } from "electron";
-import * as path from "path";
+import { app, BrowserWindow } from 'electron'
+import * as path from 'path'
+import * as pug from 'pug'
+import * as url from 'url'
 
-let mainWindow: Electron.BrowserWindow;
+let mainWindow: BrowserWindow
 
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
+    center: true,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js')
     },
     width: 800,
-  });
+    // resizable: true,
+    // frame: true,
+    // transparent: false,
+  })
+  const rp = url.parse(path.join(__dirname, '../dist/renderer'))
+  const html = pug.compileFile(path.join(__dirname, '../index.pug'))({ rendererPath: rp.href })
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  mainWindow.loadURL(`data:text/html;charset=utf-8,${encodeURI(html)}`)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on("closed", () => {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    mainWindow = null
+  })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});
+})
 
-app.on("activate", () => {
+app.on('activate', () => {
   // On OS X it"s common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
